2429726328b722836d454bf4ebf9b9834c1c8d7e
radioactivetoy <nestor.sanz@gmail.com>
2025-12-27 15:12:31 +0100

    Enable remote connection , works using ip

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,5 +1,6 @@
 export const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID || '';
-export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI || 'http://127.0.0.1:5173/callback';
+const BASE_URI = typeof window !== 'undefined' ? window.location.origin : 'http://127.0.0.1:5173';
+export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI || `${BASE_URI}/callback`;
 export const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 export const RESPONSE_TYPE = 'token';
 export const SCOPES = [
